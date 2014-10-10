4f382cd3f06af50c0b735e0bc9dfdaec61a0954a
Gordon Bleux <gordon.bleux+git@gmail.com>
2014-10-10 06:22:19 +0200

    remove even more bashism

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # a wrapper script for the Haraka server process. when invoked with
 # 'haraka' as first argument, it performs two additional steps:
@@ -28,48 +28,91 @@
 
 set -e
 
-FILENAME_UNIQUE=`date +%s`
 HARAKA_LOG="haraka-latest.log"
+HARAKA_BIN="haraka"
 
-function haraka_log_rotate() {
+# create a new log file and a symbolic link named 'haraka.log'
+# to that file in the same directory. the symbolic link is
+# overwritten if it exists.
+# @param $1 {String} log filename (without directory path)
+haraka_log_rotate() {
     # remove symlink in case of persistent mount
     test -e "$HARAKA_LOGS/haraka.log" && rm -f "$HARAKA_LOGS/haraka.log"
     touch "$HARAKA_LOGS/$1" && ln -s "$HARAKA_LOGS/$1" "$HARAKA_LOGS/haraka.log"
 }
 
-function haraka_chown() {
+# change the ownership of the haraka persistence volumes.
+# @param $1 {String} chown ownership rule
+haraka_chown() {
     chown -R "$1" "$HARAKA_LOGS" && \
     chown -R "$1" "$HARAKA_DATA"
 }
 
-function haraka_chmod() {
+# change the permission flags of the haraka persistence volumes.
+# @param $1 {String} chmod permission flags
+haraka_chmod() {
     chmod "$1" "$HARAKA_LOGS" && \
     chmod "$1" "$HARAKA_DATA"
 }
 
-function haraka_bootstrap() {
-    haraka_log_rotate "$1"
+# set the filename of the log output. if the environment variable
+# 'HARAKA_LOG_DATE_FORMAT' is set, it is used to generate the filename
+# postfix using the 'date' command. the default is to use 'date +%s' as
+# the filename postfix.
+configure_log_filename() {
+    HARAKA_LOG_IDENT=`date +%s`
+
+    if test "x$HARAKA_LOG_DATE_FORMAT" != "x";then
+        HARAKA_LOG_IDENT=`date +"$HARAKA_LOG_DATE_FORMAT"`
+    fi
+
+    HARAKA_LOG="haraka-${HARAKA_LOG_IDENT}.log"
+}
+
+# perform pre-boot actions before the SMTP server starts.
+haraka_bootstrap() {
+    configure_log_filename
+    haraka_log_rotate "$HARAKA_LOG"
 
     if test "x$DOCKER_VOLUMES_CHOWN" != "x";then
-        haraka_chown "$DOCKER_VOLUMES_CHOWN"
+        haraka_chown "$DOCKER_VOLUMES_CHOWN" || return 1
     fi
 
     if test "x$DOCKER_VOLUMES_CHMOD" != "x";then
-        haraka_chmod "$DOCKER_VOLUMES_CHMOD"
+        haraka_chmod "$DOCKER_VOLUMES_CHMOD" || return 1
+    fi
+
+    return 0
+}
+
+# ensure the environment has been set up correctly.
+# @return {Number} a value greater than zero if anything is not OK
+validate_haraka_env() {
+    if test "x$HARAKA_LOGS" = "x";then
+        echo "Haraka logs directory has not been set." 1>&2
+
+        return 1
+    fi
+
+    if test "x$HARAKA_DATA" = "x";then
+        echo "Haraka data directory has not been set." 1>&2
+
+        return 1
+    fi
+
+    return 0
+}
+
+# script entry point
+main() {
+    if test "$1" = 'haraka' -o $# -eq 0; then
+        validate_haraka_env || exit 1
+        haraka_bootstrap || exit 2
+
+        exec "$HARAKA_BIN" "$@" 2>&1 | tee "$HARAKA_LOGS/$HARAKA_LOG"
+    else
+        exec "$@"
     fi
 }
 
-
-if test "$1" = 'haraka' -o "$#" -eq 0; then
-    if test "x$HARAKA_LOG_DATE_FORMAT" != "x";then
-        FILENAME_UNIQUE=`date +"$HARAKA_LOG_DATE_FORMAT"`
-    fi
-
-    HARAKA_LOG="haraka-${FILENAME_UNIQUE}.log"
-
-    haraka_bootstrap "$HARAKA_LOG"
-
-    exec haraka "$@" 2>&1 | tee "$HARAKA_LOGS/$HARAKA_LOG"
-else
-    exec "$@"
-fi
+main "$@"
